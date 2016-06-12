865d147674b25a29c7b045b2384fd79cc2d119ed
Trisha Gee <trisha.gee@gmail.com>
2016-06-12 22:42:17 +0200

    Updated perf test to check for different numbers of entries

--- src/main/java/com/mechanitis/CollectionRefactoring1Benchmark.java
+++ src/main/java/com/mechanitis/CollectionRefactoring1Benchmark.java
@@ -4,52 +4,55 @@ import com.mechanitis.undertest.BasicDAO;
 import org.mongodb.morphia.Key;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.BenchmarkParams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 //TODO: is there a way to run the same test for 10, 100, 1000, 10_000, 100_000 entries?
+@State(Scope.Benchmark)
 public class CollectionRefactoring1Benchmark {
+    @Param({"1", "10", "100", "1000", "10000", "100000"})
+    public int numberOfItems;
+
+    private final List<Key> keys = new ArrayList<>(numberOfItems);
+    private final BasicDAO basicDAO = new BasicDAO();
+
+    @Setup()
+    public void setup(BenchmarkParams params) {
+        for (int i = 0; i < numberOfItems; i++) {
+            keys.add(new Key(String.class, "Test Collection", i));
+        }
+    }
+
     @Benchmark
     @OutputTimeUnit(MILLISECONDS)
-    public List originalIterationCode(final BenchmarkState state) {
-        return state.basicDAO.keysToIdsOriginal(state.keys);
+    public List originalIterationCode() {
+        return basicDAO.keysToIdsOriginal(keys);
 
 //        131.242 ops/ms
     }
 
     @Benchmark
     @OutputTimeUnit(MILLISECONDS)
-    public List simplifiedIterationCode(final BenchmarkState state) {
-        return state.basicDAO.keysToIdsSimplified(state.keys);
+    public List simplifiedIterationCode() {
+        return basicDAO.keysToIdsSimplified(keys);
 //85.574 ops/ms
 
     }
 
     @Benchmark
     @OutputTimeUnit(MILLISECONDS)
-    public List refactoredCode(final BenchmarkState state) {
-        return state.basicDAO.keysToIdsRefactored(state.keys);
+    public List refactoredCode() {
+        return basicDAO.keysToIdsRefactored(keys);
 // 85.045 ops/ms
 
     }
-
-    @State(Scope.Benchmark)
-    public static class BenchmarkState {
-        private final int numberOfValues = 1000;
-        private final BasicDAO basicDAO;
-        private final List<Key> keys = new ArrayList<>(numberOfValues);
-
-        public BenchmarkState() {
-            basicDAO = new BasicDAO();
-            for (int i = 0; i < numberOfValues; i++) {
-                keys.add(new Key(String.class, "Test Collection", i));
-            }
-        }
-    }
-
 }
