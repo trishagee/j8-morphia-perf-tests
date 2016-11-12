e3bb405cf4fe546edaf9b397c45b265f27f93de9
Trisha Gee <trisha.gee@gmail.com>
2016-11-12 20:31:08 +0100

    Uncommented files

--- src/main/java/com/mechanitis/ConvertersBenchmark.java
+++ src/main/java/com/mechanitis/ConvertersBenchmark.java
@@ -17,44 +17,44 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 @Warmup(iterations = 2, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 public class ConvertersBenchmark {
-//    @Param({"10", "10000"})
-//    public int numberOfItems;
-//
-//    private List<TypeConverter> source;
-//    private ConvertersUnderTest converters;
-//
-//    @Setup()
-//    public void setup(BenchmarkParams params) {
-//        source = new LinkedList<>();
-//
-//        for (int i = 0; i < numberOfItems - 1; i++) {
-//            source.add(new IdentityConverter());
-//        }
-//        source.add(new IterableConverter());
-//        converters = new ConvertersUnderTest(source);
-//    }
-//
-//    @Benchmark
-//    @OutputTimeUnit(MILLISECONDS)
-//    public TypeConverter original() {
-//        return converters.original(List.class);
-//        //126590
-//        //53732
-//    }
-//
-//    @Benchmark
-//    @OutputTimeUnit(MILLISECONDS)
-//    public TypeConverter refactored() {
-//        return converters.refactored(List.class);
-//        //12588
-//        //14823
-//    }
-//
-//    @Benchmark
-//    @OutputTimeUnit(MILLISECONDS)
-//    public TypeConverter parallel() {
-//        return converters.parallel(List.class);
-//        //
-//        //35
-//    }
+    @Param({"10", "10000"})
+    public int numberOfItems;
+
+    private List<TypeConverter> source;
+    private ConvertersUnderTest converters;
+
+    @Setup()
+    public void setup(BenchmarkParams params) {
+        source = new LinkedList<>();
+
+        for (int i = 0; i < numberOfItems - 1; i++) {
+            source.add(new IdentityConverter());
+        }
+        source.add(new IterableConverter());
+        converters = new ConvertersUnderTest(source);
+    }
+
+    @Benchmark
+    @OutputTimeUnit(MILLISECONDS)
+    public TypeConverter original() {
+        return converters.original(List.class);
+        //126590
+        //53732
+    }
+
+    @Benchmark
+    @OutputTimeUnit(MILLISECONDS)
+    public TypeConverter refactored() {
+        return converters.refactored(List.class);
+        //12588
+        //14823
+    }
+
+    @Benchmark
+    @OutputTimeUnit(MILLISECONDS)
+    public TypeConverter parallel() {
+        return converters.parallel(List.class);
+        //
+        //35
+    }
 }
--- src/main/java/com/mechanitis/TypeConverterBenchmark.java
+++ src/main/java/com/mechanitis/TypeConverterBenchmark.java
@@ -13,41 +13,41 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 @Warmup(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 public class TypeConverterBenchmark {
-//    @Param({"1", "10", "100", "1000", "10000", "100000"})
-//    public int numberOfItems;
-//
-//    public Class[] source;
-//
-//    @Setup()
-//    public void setup(BenchmarkParams params) {
-//        source = new Class[numberOfItems];
-//
-//        for (int i = 0; i < numberOfItems - 1; i++) {
-//            source[i] = EntityWith10BasicFields.class;
-//        }
-//        source[numberOfItems - 1] = String.class;
-//    }
-//
-//    @Benchmark
-//    @OutputTimeUnit(MILLISECONDS)
-//    public boolean original() {
-//        return TypeConverter.original(String.class, source);
-//        // 242613
-//        //
-//    }
-//
-//    @Benchmark
-//    @OutputTimeUnit(MILLISECONDS)
-//    public boolean refactored() {
-//        return TypeConverter.refactored(String.class, source);
-//        // 21245
-//    }
-//
-//    @Benchmark
-//    @OutputTimeUnit(MILLISECONDS)
-//    public boolean refactoredParallel() {
-//        return TypeConverter.refactoredParallel(String.class, source);
-//        // 21245
-//    }
+    @Param({"1", "10", "100", "1000", "10000", "100000"})
+    public int numberOfItems;
+
+    public Class[] source;
+
+    @Setup()
+    public void setup(BenchmarkParams params) {
+        source = new Class[numberOfItems];
+
+        for (int i = 0; i < numberOfItems - 1; i++) {
+            source[i] = EntityWith10BasicFields.class;
+        }
+        source[numberOfItems - 1] = String.class;
+    }
+
+    @Benchmark
+    @OutputTimeUnit(MILLISECONDS)
+    public boolean original() {
+        return TypeConverter.original(String.class, source);
+        // 242613
+        //
+    }
+
+    @Benchmark
+    @OutputTimeUnit(MILLISECONDS)
+    public boolean refactored() {
+        return TypeConverter.refactored(String.class, source);
+        // 21245
+    }
+
+    @Benchmark
+    @OutputTimeUnit(MILLISECONDS)
+    public boolean refactoredParallel() {
+        return TypeConverter.refactoredParallel(String.class, source);
+        // 21245
+    }
 
 }
